abbadfd44281c9c30e49544565aa67ef4c695c86
killlinux-cell <anakoisrael352@gmail.com>
2023-08-29 08:17:24 +0000

    DONE

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,8 +1,10 @@
 <template>
         <section class="hero">
         <div class="hero-content">
-            <h1 class="title">Life-Art</h1>
-            <p class="para">Découvrez un monde de créativité infinie au cœur de notre magasin d'art. Nous sommes fiers de vous offrir une expérience unique où l'inspiration se mêle à la possibilité, où chaque pinceau, chaque toile et chaque couleur deviennent les instruments de votre imagination.</p>
+            <div class="">
+                <h1 class="title text-center">Life-Art</h1>
+                <p class="para">Découvrez un monde de créativité infinie au cœur de notre magasin d'art.</p>
+            </div>
         </div>
     </section>
 </template>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
-  <header>
-   <nav>
+  	<header>
+   		<nav class="nav-container">
             <div class="logo">Life-Art</div>
             <ul class="nav-links">
                 <li><a href="#">Accueil</a></li>
@@ -8,14 +8,55 @@
                 <li><a href="#produit">Produits</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
+
+             <!-- hambuger menu -->
+			<div class="hamburger-menu-container" @click="openMenu">
+				<div
+					class="hamburger-menu"
+					:class="{ close: isActive }"
+					id="toggle-menu"
+				>111</div>
+			</div>
+			<!-- hambuger menu -->
+
+			<!-- responsive mobile -->
+			<div class="nav-mobile">
+				<span class="close-mobile-menu" @click="openMenu">X</span>
+				<a href="#" class="mobile-element" @click="openMenu">Accueil</a>
+				<a href="#service" class="mobile-element" @click="openMenu">Service</a>
+				<a href="#produit" class="mobile-element" @click="openMenu">Produits</a> 
+				<a href="#Contact" class="mobile-element" @click="openMenu">Contact</a>
+			</div>
+			<!-- responsive mobile -->
         </nav>
-    </header>
+    </header> 
+
+    
 </template>
 
 <script>
 
 export default{
   name: 'Navbar',
+   data() {
+    return {
+     isActive: false,
+    };
+},
+methods: {
+		openMenu(event) {
+			const toggleButton = document.getElementsByClassName("hamburger-menu")[0];
+			const navLinks = document.getElementsByClassName("nav-mobile")[0];
+
+			if (this.isActive == false) {
+				navLinks.style.transform = "translate(-100%, 0)";
+				this.isActive = true;
+			} else if (this.isActive == true) {
+				navLinks.style.transform = "translate(100%, 0)";
+				this.isActive = false;
+			}
+		},
+	},
 }
 </script>
 
